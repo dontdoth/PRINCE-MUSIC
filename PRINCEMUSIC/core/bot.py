120a5c7c3e362ed4facd0295c83fd11d0e8c3515
dontdothat <moha.face9@gmail.com>
2025-02-12 05:24:41 +0330

    Update bot.py

--- PRINCEMUSIC/core/bot.py
+++ PRINCEMUSIC/core/bot.py
@@ -1,10 +1,8 @@
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
-
 import config
-
 from ..logging import LOGGER
-
+import aiohttp
 
 class PRINCE(Client):
     def __init__(self):
@@ -17,9 +15,13 @@ class PRINCE(Client):
             in_memory=True,
             max_concurrent_transmissions=7,
         )
+        self.session = None  # اضافه کردن متغیر session
 
     async def start(self):
         await super().start()
+        # ایجاد یک نشست aiohttp برای کل برنامه
+        self.session = aiohttp.ClientSession()
+        
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
@@ -34,11 +36,15 @@ class PRINCE(Client):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
+            if self.session:
+                await self.session.close()
             exit()
         except Exception as ex:
             LOGGER(__name__).error(
                 f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
             )
+            if self.session:
+                await self.session.close()
             exit()
 
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
@@ -46,8 +52,13 @@ class PRINCE(Client):
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
             )
+            if self.session:
+                await self.session.close()
             exit()
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
+        # بستن نشست aiohttp قبل از توقف بات
+        if self.session:
+            await self.session.close()
         await super().stop()
