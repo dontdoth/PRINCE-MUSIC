8008284d2033a2b32716e0e71854c3722cdea488
dontdothat <moha.face9@gmail.com>
2025-02-12 09:43:59 +0330

    Update play.py

--- PRINCEMUSIC/utils/inline/play.py
+++ PRINCEMUSIC/utils/inline/play.py
@@ -1,8 +1,9 @@
 import math
-from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from PRINCEMUSIC import app
 import config
 from PRINCEMUSIC.utils.formatters import time_to_seconds
+from typing import Union
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
@@ -32,6 +33,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
+    
     if 0 < umm <= 10:
         bar = "◉—————————"
     elif 10 < umm < 20:
@@ -52,6 +54,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "————————◉—"
     else:
         bar = "—————————◉"
+
     buttons = [
         [
             InlineKeyboardButton("⏮ 30", callback_data=f"ADMIN Seek|-30|{chat_id}"),
@@ -60,6 +63,9 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton("🔇 بی‌صدا", callback_data=f"ADMIN Mute|{chat_id}"),
             InlineKeyboardButton("🔊 باصدا", callback_data=f"ADMIN Unmute|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton("🎚 تنظیم صدا", callback_data=f"VOL {chat_id}"),
         ],
         [
             InlineKeyboardButton("📋 پلی‌لیست", callback_data="get_top_playlists"),
@@ -96,17 +102,14 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton("🔊 باصدا", callback_data=f"ADMIN Unmute|{chat_id}"),
         ],
         [
+            InlineKeyboardButton("🎚 تنظیم صدا", callback_data=f"VOL {chat_id}"),
+        ],
+        [
             InlineKeyboardButton("📋 پلی‌لیست", callback_data="get_top_playlists"),
             InlineKeyboardButton("📊 آمار ویس", callback_data="vcinfo"),
         ],
         [
             InlineKeyboardButton("𝙍𝘼𝙉𝙂𝙀𝙍 ™", callback_data="logo"),
-        ],
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
         ],
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -117,6 +120,7 @@ def stream_markup(_, chat_id):
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
+
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
     buttons = [
@@ -185,5 +189,46 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
                 callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
             ),
         ],
+    ]
+    return buttons
+
+
+def volume_markup(_, chat_id):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🔈 10%",
+                callback_data=f"ADMIN Volume|10|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="🔈 25%",
+                callback_data=f"ADMIN Volume|25|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="🔉 50%",
+                callback_data=f"ADMIN Volume|50|{chat_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🔊 75%",
+                callback_data=f"ADMIN Volume|75|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="🔊 100%",
+                callback_data=f"ADMIN Volume|100|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="🔊 150%",
+                callback_data=f"ADMIN Volume|150|{chat_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🔈 صدای سفارشی",
+                callback_data=f"ADMIN CustomVolume|{chat_id}",
+            )
+        ],
+        [InlineKeyboardButton(text="🔙 برگشت", callback_data=f"MainMenu|{chat_id}")],
     ]
     return buttons
