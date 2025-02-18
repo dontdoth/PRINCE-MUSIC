f784160a25fcef3bc9bc1804900fbd95ba381cbb
dontdothat <moha.face9@gmail.com>
2025-02-19 02:04:32 +0330

    Update dicegame.py

--- PRINCEMUSIC/plugins/Yumi/dicegame.py
+++ PRINCEMUSIC/plugins/Yumi/dicegame.py
@@ -1,51 +1,41 @@
 from pyrogram import Client, enums, filters
-#from config import *
-import asyncio
 from PRINCEMUSIC import app as app
-
 from pyrogram.handlers import MessageHandler
 
-
-@app.on_message(filters.command("dice"))
+@app.on_message(filters.command("تاس"))
 async def dice(bot, message):
     x=await bot.send_dice(message.chat.id)
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"سلام {message.from_user.mention} امتیاز شما : {m}",quote=True)
   
-@app.on_message(filters.command("dart"))
+@app.on_message(filters.command("دارت")) 
 async def dart(bot, message):
     x=await bot.send_dice(message.chat.id, "🎯")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"سلام {message.from_user.mention} امتیاز شما : {m}",quote=True)
 
-@app.on_message(filters.command("basket"))
+@app.on_message(filters.command("بسکتبال"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🏀")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
-@app.on_message(filters.command("jackpot"))
-async def basket(bot, message):
+    await message.reply_text(f"سلام {message.from_user.mention} امتیاز شما : {m}",quote=True)
+
+@app.on_message(filters.command("اسلات"))
+async def jackpot(bot, message):
     x=await bot.send_dice(message.chat.id, "🎰")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
-@app.on_message(filters.command("ball"))
-async def basket(bot, message):
+    await message.reply_text(f"سلام {message.from_user.mention} امتیاز شما : {m}",quote=True)
+
+@app.on_message(filters.command("بولینگ"))
+async def bowling(bot, message):
     x=await bot.send_dice(message.chat.id, "🎳")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
-@app.on_message(filters.command("football"))
-async def basket(bot, message):
+    await message.reply_text(f"سلام {message.from_user.mention} امتیاز شما : {m}",quote=True)
+
+@app.on_message(filters.command("فوتبال"))
+async def football(bot, message):
     x=await bot.send_dice(message.chat.id, "⚽")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
-__help__ = """
- Play Game With Emojis:
-/dice - Dice 🎲
-/dart - Dart 🎯
-/basket - Basket Ball 🏀
-/ball - Bowling Ball 🎳
-/football - Football ⚽
-/jackpot - Spin slot machine 🎰
- """
+    await message.reply_text(f"سلام {message.from_user.mention} امتیاز شما : {m}",quote=True)
 
-__mod_name__ = "Dɪᴄᴇ"
+__mod_name__ = "بازی‌ها"
