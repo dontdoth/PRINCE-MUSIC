f475c1a20f10a10853d54326dc02f674b0c9f5b3
dontdothat <moha.face9@gmail.com>
2025-02-19 00:19:49 +0330

    Update Youtube.py

--- PRINCEMUSIC/platforms/Youtube.py
+++ PRINCEMUSIC/platforms/Youtube.py
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "PRINCEMUSIC/assets/cookies.txt"
+cookies_file = "PRINCEMUSIC/utils/cookies.txt"
 
 class YouTubeAPI:
     def __init__(self):
